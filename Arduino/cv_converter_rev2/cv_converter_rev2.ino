7680978505c5896d57042b54e45fb25b2406b54a
Akiyuki Okayasu <akiyuki@tkrworks.net>
2017-11-14 00:54:48 +0900

    [update] シリアル通信の規則を変更
    1つのCVにつき2byteのデータを送信する。
    上位4bitをCV出力チャンネル選択のフラグとして利用し、下位12bitをCVの値(0~4095->0V~10V)とする。

--- Arduino/cv_converter_rev2/cv_converter_rev2.ino
+++ Arduino/cv_converter_rev2/cv_converter_rev2.ino
@@ -10,31 +10,58 @@ void setup() {
   // put your setup code here, to run once:
   delay(100);
   Serial.begin(57600);//74800 115200
-  for(int i = 0; i < 4; i++){
+  for (int i = 0; i < 4; i++) {
     pinMode(cs[i], OUTPUT);
-//    Serial.println(i);
+    //    Serial.println(i);
   }
   pinMode(sck, OUTPUT);
   pinMode(sdi, OUTPUT);
-  pinMode(ldac, OUTPUT); 
+  pinMode(ldac, OUTPUT);
   Serial.println("Block modular !");
 }
 
 void loop() {
-  if(Serial.available() > 16){
-    char head = Serial.read();
-    if(head == 104)
+  if (Serial.available() > 1) //more than 2bytes
+  {
+    byte twobytes[];
+    Serial.readBytes(twobytes, 2);
+    uint8_t boardIndex = twobytes[0] >> 6 & B00000011;
+    uint8_t channel = bitRead(twobytes[0], 5);
+    word da_value =  word(twobyte[0] & B00001111, twobyte[1]);
+    if (0 <= da_value && da_value < 4096)
     {
-      for(int i = 0; i < cvBoardCount; i++)
-      {
-          cvOut2channel(cs[i]);
-      }
+      digitalWrite(ldac, HIGH);
+      digitalWrite(cs[boardIndex], LOW);
+      DACout(sdi, sck, channel, da_value);
+      digitalWrite(cs[boardIndex], HIGH);
+      digitalWrite(ldac, LOW);
     }
   }
 }
 
+void DACout(int dataPin, int clockPin, int destination, int value)
 {
+  int i ;
 
+  // コマンドデータの出力
+  digitalWrite(dataPin, destination) ; // 出力するピン(OUTA/OUTB)を選択する
+  digitalWrite(clockPin, HIGH) ;
+  digitalWrite(clockPin, LOW) ;
+  digitalWrite(dataPin, LOW) ;       // VREFバッファは使用しない
+  digitalWrite(clockPin, HIGH) ;
+  digitalWrite(clockPin, LOW) ;
+  digitalWrite(dataPin, HIGH) ;      // 出力ゲインは１倍とする
+  digitalWrite(clockPin, HIGH) ;
+  digitalWrite(clockPin, LOW) ;
+  digitalWrite(dataPin, HIGH) ;      // アナログ出力は有効とする
+  digitalWrite(clockPin, HIGH) ;
+  digitalWrite(clockPin, LOW) ;
+  // ＤＡＣデータビット出力
+  for (i = 11 ; i >= 0 ; i--) {
+    if (((value >> i) & 0x1) == 1) digitalWrite(dataPin, HIGH) ;
+    else                           digitalWrite(dataPin, LOW) ;
+    digitalWrite(clockPin, HIGH) ;
+    digitalWrite(clockPin, LOW) ;
   }
 }
 

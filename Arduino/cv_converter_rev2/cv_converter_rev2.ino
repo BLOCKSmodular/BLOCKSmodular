7ab14cee6a00a9e138bfd6ba82a06133bef73273
Akiyuki Okayasu <akiyuki@tkrworks.net>
2017-11-14 00:50:20 +0900

    [remove] concatValues()とcvOut2channel()を削除

--- Arduino/cv_converter_rev2/cv_converter_rev2.ino
+++ Arduino/cv_converter_rev2/cv_converter_rev2.ino
@@ -33,54 +33,9 @@ void loop() {
   }
 }
 
-void cvOut2channel(int cspin)
 {
-  for(int channel = 0 ; channel < 2; channel ++){
 
-     uint8_t value_high = Serial.read();
-     uint8_t value_low = Serial.read();
-     uint16_t concat_value = concatValues(value_high, value_low);
-     uint16_t da_value = map(concat_value, 0, 65535, 0, 4095);
-     digitalWrite(ldac, HIGH);
-     digitalWrite(cspin, LOW);
-     DACout(sdi, sck, channel, da_value);
-     digitalWrite(cspin, HIGH);
-     digitalWrite(ldac,LOW);
-     delay(3);
   }
-}
-
-uint16_t concatValues( uint8_t value_high, uint8_t value_low)
-{
-  uint16_t val = (value_high << 8) | (value_low & 0b11111111);
-  return val;
-}
-
-
-void DACout(int dataPin,int clockPin,int destination,int value)
-{
-     int i ;
-
-     // コマンドデータの出力
-     digitalWrite(dataPin,destination) ;// 出力するピン(OUTA/OUTB)を選択する
-     digitalWrite(clockPin,HIGH) ;
-     digitalWrite(clockPin,LOW) ;
-     digitalWrite(dataPin,LOW) ;        // VREFバッファは使用しない
-     digitalWrite(clockPin,HIGH) ;
-     digitalWrite(clockPin,LOW) ;
-     digitalWrite(dataPin,HIGH) ;       // 出力ゲインは１倍とする
-     digitalWrite(clockPin,HIGH) ;
-     digitalWrite(clockPin,LOW) ;
-     digitalWrite(dataPin,HIGH) ;       // アナログ出力は有効とする
-     digitalWrite(clockPin,HIGH) ;
-     digitalWrite(clockPin,LOW) ;
-     // ＤＡＣデータビット出力
-     for (i=11 ; i>=0 ; i--) {
-          if (((value >> i) & 0x1) == 1) digitalWrite(dataPin,HIGH) ;
-          else                           digitalWrite(dataPin,LOW) ;
-          digitalWrite(clockPin,HIGH) ;
-          digitalWrite(clockPin,LOW) ;
-     }
 }
 
 

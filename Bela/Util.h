433f84ab2dbd974f3474cb3aff21b2442b0828f7
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2018-03-31 00:05:55 +0900

    [add] Pi/2の定数を追加

--- Bela/Util.h
+++ Bela/Util.h
@@ -8,8 +8,9 @@
 #include <iostream>
 #include <cstdlib>
 
-static constexpr float twoPi = 6.28318530718f;
 static constexpr float Pi = 3.14159265359f;
+static constexpr float twoPi = 6.28318530718f;
+static constexpr float halfPi = 1.57079632679f;
 
 static inline int getNumChannels(std::string file)
 {

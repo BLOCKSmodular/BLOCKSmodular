2ae3815f9cbc1f3ec92ae4d6d681d3f10737e5b2
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2018-04-19 10:50:23 +0900

    Clean: インデント調整とコメントアウトの削除

--- Bela/render.cpp
+++ Bela/render.cpp
@@ -52,13 +52,13 @@ Smoothing CVSmooth[NUMCVOUT];
 
 void midiMessageCallback(MidiChannelMessage message, void *arg)
 {
-	const int channel = message.getChannel();//MIDIChannel 0~15
-	
-	//Note On
-	if(message.getType() == kmmNoteOn){
-	}
-
-	//Control change
+    const int channel = message.getChannel();//MIDIChannel 0~15
+    
+    //Note On
+    if(message.getType() == kmmNoteOn){
+    }
+    
+    //Control change
     if(message.getType() == kmmControlChange)
     {
         const int controlNum = message.getDataByte(0);
@@ -100,17 +100,17 @@ void midiMessageCallback(MidiChannelMessage message, void *arg)
                 case AudioModeB: {
                     //Sample playback
                     if(controlNum == 1) {
-                    	if(value == 127) {
-                    		samplePlay_buffer[voiceIndex].setReadIter(0);
-                    		samplePlay_isPlaying[voiceIndex] = true;
-                    	}
+                        if(value == 127) {
+                            samplePlay_buffer[voiceIndex].setReadIter(0);
+                            samplePlay_isPlaying[voiceIndex] = true;
+                        }
                     }
                     break;
                 }
                 case AudioModeC: {
                     //Karplus strong
                     if(controlNum == 1 || controlNum == 2) {
-                    	bool isUpeerByte{controlNum == 1};
+                        bool isUpeerByte{controlNum == 1};
                         kp_pitch[voiceIndex].set(value, isUpeerByte);
                         if(kp_pitch[voiceIndex].update()) {
                         		const float p = kp_pitch[voiceIndex].get() * 40.0f + 40.0f;//40Hz~80Hz
@@ -119,13 +119,13 @@ void midiMessageCallback(MidiChannelMessage message, void *arg)
                     }
                     
                     if(controlNum == 3 || controlNum == 4) {
-                    	bool isUpeerByte{controlNum == 3};
+                        bool isUpeerByte{controlNum == 3};
                         kp_decay[voiceIndex].set(value, isUpeerByte);
                         if(kp_decay[voiceIndex].update()) karplus[voiceIndex].setDecay(kp_decay[voiceIndex].get());
                     }
-                    	
+                    
                     if(controlNum == 5 && value == 127) {
-                    	karplus[voiceIndex].trigger();
+                        karplus[voiceIndex].trigger();
                     }
                     
                     break;
@@ -133,30 +133,30 @@ void midiMessageCallback(MidiChannelMessage message, void *arg)
                 case AudioModeD: {
                     //Logistic map
                     if(voiceIndex == 0) {
-                    	if(controlNum == 1 || controlNum == 2) {
-                        	bool isUpeerByte{controlNum == 1};
-                        	lgst_alpha.set(value, isUpeerByte);
-                        	if(lgst_alpha.update()) logisticOsc.setAlpha(lgst_alpha.get() * 0.5f + 3.490f);
-                    	}
-                    
-                    	if(controlNum == 3 || controlNum == 4) {
-                        	bool isUpeerByte{controlNum == 3};
-                        	lgst_gain.set(value, isUpeerByte);
-                        	if(lgst_gain.update()) logisticOsc.setGain(lgst_gain.get());
-                    	}
+                        if(controlNum == 1 || controlNum == 2) {
+                            bool isUpeerByte{controlNum == 1};
+                            lgst_alpha.set(value, isUpeerByte);
+                            if(lgst_alpha.update()) logisticOsc.setAlpha(lgst_alpha.get() * 0.5f + 3.490f);
+                        }
+                        
+                        if(controlNum == 3 || controlNum == 4) {
+                            bool isUpeerByte{controlNum == 3};
+                            lgst_gain.set(value, isUpeerByte);
+                            if(lgst_gain.update()) logisticOsc.setGain(lgst_gain.get());
+                        }
                     }
                     else if(voiceIndex == 1) {
-                    	if(controlNum == 1 || controlNum == 2) {
-                        	bool isUpeerByte{controlNum == 1};
-                        	sineCircle_k.set(value, isUpeerByte);
-                        	if(sineCircle_k.update()) sineCircleOsc.setK(sineCircle_k.get() * 0.2f + 1.0f);
-                    	}
-                    
-                    	if(controlNum == 3 || controlNum == 4) {
-                        	bool isUpeerByte{controlNum == 3};
-                        	sineCircle_gain.set(value, isUpeerByte);
-                        	if(sineCircle_gain.update()) sineCircleOsc.setGain(sineCircle_gain.get());
-                    	}
+                        if(controlNum == 1 || controlNum == 2) {
+                            bool isUpeerByte{controlNum == 1};
+                            sineCircle_k.set(value, isUpeerByte);
+                            if(sineCircle_k.update()) sineCircleOsc.setK(sineCircle_k.get() * 0.2f + 1.0f);
+                        }
+                        
+                        if(controlNum == 3 || controlNum == 4) {
+                            bool isUpeerByte{controlNum == 3};
+                            sineCircle_gain.set(value, isUpeerByte);
+                            if(sineCircle_gain.update()) sineCircleOsc.setGain(sineCircle_gain.get());
+                        }
                     }
                     break;
                 }
@@ -176,11 +176,11 @@ void midiMessageCallback(MidiChannelMessage message, void *arg)
                 }
                 case CVModeB: {
                     //Microtonal
-                	if(voiceIndex >= 4) {
-                		std::cout<<"MIDI: Invalid voice number"<<std::endl;
-                		break;
-                	}
-                	
+                    if(voiceIndex >= 4) {
+                        std::cout<<"MIDI: Invalid voice number"<<std::endl;
+                        break;
+                    }
+                    
                     if(controlNum == 1 || controlNum == 2) {
                         bool isUpeerByte{controlNum == 1};
                         microtone_Distance[voiceIndex].set(value, isUpeerByte);
@@ -248,35 +248,37 @@ bool setup(BelaContext *context, void *userData)
 
 void render(BelaContext *context, void *userData)
 {
-	//---------------------------------------
-	//test用強制モード切替
-	// AudiomodeFlag = AudioModeB;
+    //---------------------------------------
+    //test用強制モード切替
+    // AudiomodeFlag = AudioModeB;
     // midi_byte_t audioModeBytes[3] = {0xBF, (midi_byte_t)(1), 48};//Channel:16, CC Number:1
     //	midi.writeOutput(audioModeBytes, 3);
     //	CVmodeFlag = CVModeB;
     //	midi_byte_t cvModeBytes[3] = {0xBF, (midi_byte_t)(2), 48};//Channel:16, CC Number:2
     //	midi.writeOutput(cvModeBytes, 3);
- 	//----------------------------------------
-	
-/*===========================================
-Digital
-=============================================*/
-	bool cvpage = (bool)digitalRead(context, 0, P9_12);
-	if(cvpage != isCVPage) {
+    //----------------------------------------
+    
+    
+    
+    /*===========================================
+     Digital
+     =============================================*/
+    bool cvpage = (bool)digitalRead(context, 0, P9_12);
+    if(cvpage != isCVPage) {
         midi_byte_t bp[3] = {0xBF, (midi_byte_t)(8), 0};//Channel:16, CC Number:8
         if(cvpage) {
-        	bp[2] = 127;
+            bp[2] = 127;
         }
         else {
-        	bp[2] = 0;
+            bp[2] = 0;
         }
         midi.writeOutput(bp, 3);
         isCVPage = cvpage;
-	}
-
+    }
+    
     unsigned char audioFLG = 0;
     unsigned char cvFLG = 0;
-
+    
     //Audio
     if(digitalRead(context, 0, P8_07)) audioFLG = AudioModeA;
     if(digitalRead(context, 0, P8_09)) audioFLG = AudioModeB;
@@ -331,13 +333,13 @@ Digital
         CVmodeFlag = cvFLG;
     }
     
-/*===========================================
-Analogue
-=============================================*/
-//AnalogueIN
+    /*===========================================
+     Analogue
+     =============================================*/
+    //AnalogueIN
     const int numAnalogueFrames = context->analogFrames;
     
-//AnalogueOUT
+    //AnalogueOUT
     switch(CVmodeFlag) {
         case CVModeA: {
             //Morph looper
@@ -360,15 +362,15 @@ Analogue
             break;
         }
         default: {
-            // rt_printf("CVMode: %d\n", CVmodeFlag);
+            rt_printf("CVMode: %d\n", CVmodeFlag);
             break;
         }
     }
     
     
-/*===========================================
-Audio
-=============================================*/
+    /*===========================================
+     Audio
+     =============================================*/
     const int numAudioFrames = context->audioFrames;
     float gr[numAudioFrames];
     float l[numAudioFrames];
@@ -376,127 +378,51 @@ Audio
     float kpbuf[numAudioFrames];
     float noiseBuf[numAudioFrames];
     for(unsigned int i = 0; i < numAudioFrames; ++i) {
-    	gr[i] = 0.0f;
-    	l[i] = 0.0f;
-    	r[i] = 0.0f;
-    	kpbuf[i] = 0.0f;
-    	noiseBuf[i] = 0.0f;
+        gr[i] = 0.0f;
+        l[i] = 0.0f;
+        r[i] = 0.0f;
+        kpbuf[i] = 0.0f;
+        noiseBuf[i] = 0.0f;
     }
     
     //granular
-   	granular.nextBlock(gr, numAudioFrames);
-   	
-   	//Sample playback
+    granular.nextBlock(gr, numAudioFrames);
+    
+    //Sample playback
     for(unsigned int sample = 0; sample < numAudioFrames; ++sample) {
-    	l[sample] = 0.0f;
+        l[sample] = 0.0f;
         r[sample] = 0.0f;
     }
-            
+    
     for(unsigned int i = 0; i < 4; ++i) {
-    	if(samplePlay_isPlaying[i]) {
-        	samplePlay_buffer[i].nextBlock(l, r, numAudioFrames);
-        	if(samplePlay_buffer[i].isBufferEnd()) samplePlay_isPlaying[i] = false;
+        if(samplePlay_isPlaying[i]) {
+            samplePlay_buffer[i].nextBlock(l, r, numAudioFrames);
+            if(samplePlay_buffer[i].isBufferEnd()) samplePlay_isPlaying[i] = false;
         }
-   	}
-   	
-   	//Karplus Strong
-	for(unsigned int i = 0; i < NUMKARPLUSVOICE; ++i) {
-		karplus[i].nextBlock(kpbuf, numAudioFrames);
-  	}
-
-
-  	//Logistic map  	
-  	for(unsigned int sample = 0; sample < numAudioFrames; ++sample) {
+    }
+    
+    //Karplus Strong
+    for(unsigned int i = 0; i < NUMKARPLUSVOICE; ++i) {
+        karplus[i].nextBlock(kpbuf, numAudioFrames);
+    }
+    
+    
+    //Logistic map
+    for(unsigned int sample = 0; sample < numAudioFrames; ++sample) {
         float v = 0.0f;
         v += logisticOsc.update();
         v += sineCircleOsc.update();
         noiseBuf[sample] = v;
-  	}
-     
-     
-    for(unsigned int i = 0; i < numAudioFrames; ++i) {
-    	audioWrite(context, i, 0, gr[i] * 0.4f + l[i] + kpbuf[i] * 0.3f + noiseBuf[i] * 0.5f);
-    	audioWrite(context, i, 1, gr[i] * 0.4f + r[i] + kpbuf[i] * 0.3f + noiseBuf[i] * 0.5f);
-  	}   
+    }
     
-    // switch(AudiomodeFlag) {
-    //     case AudioModeA: {
-    //         //Granular
-    //         float gr[numAudioFrames];
-    //         for(unsigned int i = 0; i < numAudioFrames; ++i) {
-    //             gr[i] = 0.0f;
-    //         }
-    //         granular.nextBlock(gr, numAudioFrames);
-            
-    //         for(unsigned int i = 0; i < numAudioFrames; ++i) {
-    //         	const float gain = 0.01f;
-    //             audioWrite(context, i, 0, gr[i] * gain);
-    //             audioWrite(context, i, 1, gr[i] * gain);
-    //         }
-    //         break;
-    //     }
-    //     case AudioModeB: {
-    //         //Sample playback
-    //         float l[numAudioFrames];
-    //         float r[numAudioFrames];
-    //         for(unsigned int sample = 0; sample < numAudioFrames; ++sample) {
-    //         	l[sample] = 0.0f;
-    //         	r[sample] = 0.0f;
-    //         }
-            
-    //         for(unsigned int i = 0; i < 4; ++i) {
-    //         	if(samplePlay_isPlaying[i]) {
-    //         		samplePlay_buffer[i].nextBlock(l, r, numAudioFrames);
-    //         		if(samplePlay_buffer[i].isBufferEnd()) samplePlay_isPlaying[i] = false;
-            		
-    //         	}
-    //         }
-            
-    //         for(unsigned int sample = 0; sample < numAudioFrames; ++sample) {
-    //         	const float gain = 0.1f;
-    //             audioWrite(context, sample, 0, l[sample] * gain);
-    //             audioWrite(context, sample, 1, r[sample] * gain);
-    //         }
-    //         break;
-    //     }
-    //     case AudioModeC: {
-    //         //Karplus strong
-    //         float b[numAudioFrames];
-    //         for(unsigned int i = 0; i < numAudioFrames; ++i) {
-    //             b[i] = 0.0f;
-    //         }
-            
-    //         for(unsigned int i = 0; i < NUMKARPLUSVOICE; ++i) {
-    //         	karplus[i].nextBlock(b, numAudioFrames);
-    //         }
-            
-    //         for(unsigned int i = 0; i < numAudioFrames; ++i) {
-    //         	const float v = b[i] * 0.1f;
-    //             audioWrite(context, i, 0, v);
-    //             audioWrite(context, i, 1, v);
-    //         }
-    //         break;
-    //     }
-    //     case AudioModeD: {
-    //         //Logistic map
-    //         for(unsigned int sample = 0; sample < numAudioFrames; ++sample) {
-    //         	float v = 0.0f;
-    //         	v += logisticOsc.update();
-    //         	v += sineCircleOsc.update();
-    //         	v = v * 0.1f;
-    //             audioWrite(context, sample, 0, v);
-    //             audioWrite(context, sample, 1, v);
-    //         }
-    //         break;
-    //     }
-    //     default: {
-    //         // rt_printf("AudioMode: %d\n", AudiomodeFlag);
-    //         break;
-    //     }
-    // }
+    
+    for(unsigned int i = 0; i < numAudioFrames; ++i) {
+        audioWrite(context, i, 0, gr[i] * 0.4f + l[i] + kpbuf[i] * 0.3f + noiseBuf[i] * 0.5f);
+        audioWrite(context, i, 1, gr[i] * 0.4f + r[i] + kpbuf[i] * 0.3f + noiseBuf[i] * 0.5f);
+    }
 }
 
 void cleanup(BelaContext *context, void *userData)
 {
-	delete [] samplePlay_buffer;
+    delete [] samplePlay_buffer;
 }

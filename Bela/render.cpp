0142f6dfe2eb700e63cbe1cdbca8069b0ef5894e
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2018-05-16 16:05:15 +0900

    Update: v0.8に向けてモード整理、定数名の変更

--- Bela/render.cpp
+++ Bela/render.cpp
@@ -27,22 +27,20 @@
 #define pin_gate2 P8_10
 #define pin_gate3 P8_12
 #define pin_gate4 P8_16
+static constexpr int NumOutput_CV = 8;
+static constexpr int NumOutput_Gate = 4;
+static constexpr int NumVoice_PhysicalDrum = 4;
+static constexpr int NumVoice_Microtone = 4;
+
 enum class ModeList{
     init = 0,
-    Granular,
-    SamplePlay,
-    Karplus,
-    Logistic,
-    SineCircle,
-    MorphLooper,
-    Microtonal,
-    Euclid
+    Microtone
+    Euclid,
+    ChaoticNoise,
+    PhysicalDrum,
+    Granular
 };
 
-static constexpr int NUMCVOUT = 8;
-static constexpr int NUMSAMPLEPLAYBUFFER = 4;
-static constexpr int NUMKARPLUSVOICE = 4;
-static constexpr int NUMMICROTONALVOICE = 4;
 Midi midi;
 ModeList mode;
 const char *gMidiPort0 = "hw:1,0,0";
@@ -50,21 +48,16 @@ const char *gMidiPort0 = "hw:1,0,0";
 LogisticMap logisticOsc;
 HighResolutionControlChange	lgst_alpha;
 HighResolutionControlChange lgst_gain;
-SineCircleMap sineCircleOsc;
-HighResolutionControlChange sineCircle_k;
-HighResolutionControlChange sineCircle_gain;
 GranularSynth granular;
 HighResolutionControlChange gr_Position[2];
 HighResolutionControlChange gr_GrainSize[2];
 HighResolutionControlChange gr_WindowShape[2];
-HighResolutionControlChange microtone_Distance[NUMMICROTONALVOICE];
-HighResolutionControlChange microtone_Pressure[NUMMICROTONALVOICE];
-StereoBuffer* samplePlay_buffer;
-bool samplePlay_isPlaying[NUMSAMPLEPLAYBUFFER]{false, false, false, false};
-KarplusStrong karplus[NUMKARPLUSVOICE];
-HighResolutionControlChange kp_pitch[NUMKARPLUSVOICE];
-HighResolutionControlChange kp_decay[NUMKARPLUSVOICE];
-Smoothing CVSmooth[NUMCVOUT];
+HighResolutionControlChange microtone_Distance[NumVoice_Microtone];
+HighResolutionControlChange microtone_Pressure[NumVoice_Microtone];
+KarplusStrong karplus[NumVoice_PhysicalDrum];
+HighResolutionControlChange kp_pitch[NumVoice_PhysicalDrum];
+HighResolutionControlChange kp_decay[NumVoice_PhysicalDrum];
+Smoothing CVSmooth[NumOutput_CV];
 
 void midiMessageCallback(MidiChannelMessage message, void *arg)
 {
@@ -76,13 +69,78 @@ void midiMessageCallback(MidiChannelMessage message, void *arg)
         //std::cout<<channel<<", "<<controlNum<<", "<<value<<std::endl;
         
         switch(mode) {
-            case ModeList::init: {
-                std::cout<<"mode init..."<<std::endl;
+            case ModeList::Microtone: {
+                if(channel >= NumVoice_Microtone) {
+                    std::cout<<"MIDI: Invalid voice number"<<std::endl;
+                    break;
+                }
+                
+                if(controlNum == 1 || controlNum == 2) {
+                    bool isUpeerByte{controlNum == 1};
+                    microtone_Distance[channel].set(value, isUpeerByte);
+                    if(microtone_Distance[channel].update()) CVSmooth[channel * 2].set(microtone_Distance[channel].get());
+                }
+                
+                if(controlNum == 3 || controlNum == 4) {
+                    bool isUpeerByte{controlNum == 3};
+                    microtone_Pressure[channel].set(value, isUpeerByte);
+                    if(microtone_Pressure[channel].update()) CVSmooth[channel * 2 + 1].set(microtone_Pressure[channel].get());
+                }
                 break;
             }
+                
+            case ModeList::Euclid: {
+                break;
+            }
+                
+                
+            case ModeList::ChaoticNoise: {
+                if(channel == 0) {//TODO MIDIchを1~4まで対応
+                    if(controlNum == 1 || controlNum == 2) {
+                        bool isUpeerByte{controlNum == 1};
+                        lgst_alpha.set(value, isUpeerByte);
+                        if(lgst_alpha.update()) logisticOsc.setAlpha(lgst_alpha.get() * 0.5f + 3.490f);
+                    }
+                    
+                    if(controlNum == 3 || controlNum == 4) {
+                        bool isUpeerByte{controlNum == 3};
+                        lgst_gain.set(value, isUpeerByte);
+                        if(lgst_gain.update()) logisticOsc.setGain(lgst_gain.get());
+                    }
+                }
+                break;
+            }
+                
+            case ModeList::PhysicalDrum: {
+                if(channel >= NumVoice_PhysicalDrum) {
+                    break;
+                }
+                
+                if(controlNum == 1 || controlNum == 2) {
+                    bool isUpeerByte{controlNum == 1};
+                    kp_pitch[channel].set(value, isUpeerByte);
+                    if(kp_pitch[channel].update()) {
+                        const float p = kp_pitch[channel].get() * 40.0f + 40.0f;//40Hz~80Hz
+                        karplus[channel].setFreq(p);
+                    }
+                }
+                
+                if(controlNum == 3 || controlNum == 4) {
+                    bool isUpeerByte{controlNum == 3};
+                    kp_decay[channel].set(value, isUpeerByte);
+                    if(kp_decay[channel].update()) karplus[channel].setDecay(kp_decay[channel].get());
+                }
+                
+                if(controlNum == 5 && value == 127) {
+                    karplus[channel].trigger();
+                }
+                
+                break;
+            }
+                
+                
             case ModeList::Granular: {
-                if(channel >= 2)
-                {
+                if(channel >= 2) {
                     break;
                 }
                 
@@ -110,106 +168,7 @@ void midiMessageCallback(MidiChannelMessage message, void *arg)
                 
                 break;
             }
-            case ModeList::SamplePlay: {
-                if(channel >= NUMSAMPLEPLAYBUFFER)
-                {
-                    break;
-                }
                 
-                if(controlNum == 1) {
-                    if(value == 127) {
-                        samplePlay_buffer[channel].setReadIter(0);
-                        samplePlay_isPlaying[channel] = true;
-                    }
-                }
-                
-                break;
-            }
-            case ModeList::Karplus: {
-                if(channel >= NUMKARPLUSVOICE)
-                {
-                    break;
-                }
-                
-                if(controlNum == 1 || controlNum == 2) {
-                    bool isUpeerByte{controlNum == 1};
-                    kp_pitch[channel].set(value, isUpeerByte);
-                    if(kp_pitch[channel].update()) {
-                        const float p = kp_pitch[channel].get() * 40.0f + 40.0f;//40Hz~80Hz
-                        karplus[channel].setFreq(p);
-                    }
-                }
-                
-                if(controlNum == 3 || controlNum == 4) {
-                    bool isUpeerByte{controlNum == 3};
-                    kp_decay[channel].set(value, isUpeerByte);
-                    if(kp_decay[channel].update()) karplus[channel].setDecay(kp_decay[channel].get());
-                }
-                
-                if(controlNum == 5 && value == 127) {
-                    karplus[channel].trigger();
-                }
-                
-                break;
-            }
-            case ModeList::Logistic: {
-                if(channel == 0) {
-                    if(controlNum == 1 || controlNum == 2) {
-                        bool isUpeerByte{controlNum == 1};
-                        lgst_alpha.set(value, isUpeerByte);
-                        if(lgst_alpha.update()) logisticOsc.setAlpha(lgst_alpha.get() * 0.5f + 3.490f);
-                    }
-                    
-                    if(controlNum == 3 || controlNum == 4) {
-                        bool isUpeerByte{controlNum == 3};
-                        lgst_gain.set(value, isUpeerByte);
-                        if(lgst_gain.update()) logisticOsc.setGain(lgst_gain.get());
-                    }
-                }
-                else if(channel == 1) {
-                    if(controlNum == 1 || controlNum == 2) {
-                        bool isUpeerByte{controlNum == 1};
-                        sineCircle_k.set(value, isUpeerByte);
-                        if(sineCircle_k.update()) sineCircleOsc.setK(sineCircle_k.get() * 0.2f + 1.0f);
-                    }
-                    
-                    if(controlNum == 3 || controlNum == 4) {
-                        bool isUpeerByte{controlNum == 3};
-                        sineCircle_gain.set(value, isUpeerByte);
-                        if(sineCircle_gain.update()) sineCircleOsc.setGain(sineCircle_gain.get());
-                    }
-                }
-                break;
-            }
-            case ModeList::SineCircle: {
-                //TODO SineCircleMapモードの実装
-                break;
-            }
-            case ModeList::MorphLooper: {
-                break;
-            }
-            case ModeList::Microtonal: {
-                if(channel >= NUMMICROTONALVOICE) {
-                    std::cout<<"MIDI: Invalid voice number"<<std::endl;
-                    break;
-                }
-                
-                if(controlNum == 1 || controlNum == 2) {
-                    bool isUpeerByte{controlNum == 1};
-                    microtone_Distance[channel].set(value, isUpeerByte);
-                    if(microtone_Distance[channel].update()) CVSmooth[channel * 2].set(microtone_Distance[channel].get());
-                }
-                
-                if(controlNum == 3 || controlNum == 4) {
-                    bool isUpeerByte{controlNum == 3};
-                    microtone_Pressure[channel].set(value, isUpeerByte);
-                    if(microtone_Pressure[channel].update()) CVSmooth[channel * 2 + 1].set(microtone_Pressure[channel].get());
-                }
-                break;
-            }
-            case ModeList::Euclid: {
-                break;
-            }
             default: {
                 break;
             }
@@ -219,10 +178,7 @@ void midiMessageCallback(MidiChannelMessage message, void *arg)
 
 bool setup(BelaContext *context, void *userData)
 {
-    //Sleep for waiting boot BLOCKS
-    std::cout<<"Begin sleep"<<std::endl;
-    sleep(2);
-    std::cout<<"End sleep"<<std::endl;
+    mode = ModeList::init;
     
     //Mode change pin setup
     pinMode(context, 0, pin_microtone, INPUT);//Microtone
@@ -243,13 +199,7 @@ bool setup(BelaContext *context, void *userData)
     midi.enableParser(true);
     midi.setParserCallback(midiMessageCallback, (void *)gMidiPort0);
     
-    samplePlay_buffer = new StereoBuffer[NUMSAMPLEPLAYBUFFER];
-    
-    //Load Sample
-    samplePlay_buffer[0].loadSampleFile("samplePlayA.wav");
-    samplePlay_buffer[1].loadSampleFile("samplePlayB.wav");
-    samplePlay_buffer[2].loadSampleFile("samplePlayC.wav");
-    samplePlay_buffer[3].loadSampleFile("samplePlayD.wav");
+    //Load Sample file
     granular.loadFile("GranularSource.wav");
     
     return true;
@@ -257,26 +207,16 @@ bool setup(BelaContext *context, void *userData)
 
 void render(BelaContext *context, void *userData)
 {
-    //---------------------------------------
-    //test用強制モード切替
-    // AudiomodeFlag = AudioModeB;
-    // midi_byte_t audioModeBytes[3] = {0xBF, (midi_byte_t)(1), 48};//Channel:16, CC Number:1
-    //	midi.writeOutput(audioModeBytes, 3);
-    //	CVmodeFlag = CVModeB;
-    //	midi_byte_t cvModeBytes[3] = {0xBF, (midi_byte_t)(2), 48};//Channel:16, CC Number:2
-    //	midi.writeOutput(cvModeBytes, 3);
-    //----------------------------------------
-    
     /*===========================================
-     Digital IN: Mode change
+     Mode change
      =============================================*/
-    int modeFlag = 0;
     int* modeFlag = nullptr;
     if(digitalRead(context, 0, pin_microtone)) modeFlag = static_cast<int*>ModeList::Microtone;
     if(digitalRead(context, 0, pin_euclid)) modeFlag = static_cast<int*>ModeList::Euclid;
     if(digitalRead(context, 0, pin_chaoticNoise)) modeFlag = static_cast<int*>ModeList::ChaoticNoise;
     if(digitalRead(context, 0, pin_physicalDrum)) modeFlag = static_cast<int*>ModeList::PhysicalDrum;
     if(digitalRead(context, 0, pin_granular)) modeFlag = static_cast<int*>ModeList::Granular;
+    if(mode != static_cast<ModeList>(modeFlag) && modeFlag != nullptr) {
         midi_byte_t bytes[3] = {0xBF, (midi_byte_t)(1), 0};//Channel:16, CC Number:1
         bytes[2] = modeFlag;
         midi.writeOutput(bytes, 3);
@@ -284,26 +224,20 @@ void render(BelaContext *context, void *userData)
     }
     
     /*===========================================
-     Analogue OUT: CV/Gate
+     CV Output
      =============================================*/
     const int numAnalogueFrames = context->analogFrames;
     switch(mode) {
-        case ModeList::MorphLooper: {
-            break;
-        }
-        case ModeList:: Microtonal: {
+        case ModeList:: Microtone: {
             for(unsigned int n = 0; n < numAnalogueFrames; n++) {
-                for(unsigned ch = 0; ch < NUMCVOUT; ch++) {
+                for(unsigned ch = 0; ch < NumOutput_CV; ch++) {
                     analogWrite(context, n, ch, CVSmooth[ch].getNextValue());
                 }
             }
             break;
         }
-        case ModeList:: Euclid: {
-            break;
-        }
         default: {
-            rt_printf("CVOut Switch: default...\n");
+            //rt_printf("CVOut Switch: default...\n");
             break;
         }
     }
@@ -326,13 +260,7 @@ void render(BelaContext *context, void *userData)
     }
     
     granular.nextBlock(grBuf, numAudioFrames);//granular
-    for(unsigned int i = 0; i < 4; ++i) {//Sample playback
-        if(samplePlay_isPlaying[i]) {
-            samplePlay_buffer[i].nextBlock(sampLeftBuf, sampRightBuf, numAudioFrames);
-            if(samplePlay_buffer[i].isBufferEnd()) samplePlay_isPlaying[i] = false;
-        }
-    }
-    for(unsigned int i = 0; i < NUMKARPLUSVOICE; ++i) {//Karplus Strong
+    for(unsigned int i = 0; i < NumVoice_PhysicalDrum; ++i) {//Karplus Strong
         karplus[i].nextBlock(kpBuf, numAudioFrames);
     }
     for(unsigned int sample = 0; sample < numAudioFrames; ++sample) {//Logistic map
@@ -347,5 +275,4 @@ void render(BelaContext *context, void *userData)
 
 void cleanup(BelaContext *context, void *userData)
 {
-    delete [] samplePlay_buffer;
 }

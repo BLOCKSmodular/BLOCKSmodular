22cdda114b239dd9817bd793e58c23673a0db90b
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2018-05-17 09:35:42 +0900

    Update: EuclideanRhythm.hのテンポ可変など大きな改変

--- Bela/EuclideanRhythm.h
+++ Bela/EuclideanRhythm.h
@@ -14,18 +14,13 @@ class EuclideanRhythm(){
 public:
     EuclideanRhythm();
     ~EuclideanRhythm();
-    void generateRhythm(const int numSteps, const int numBeats)
+    void generateRhythm()
     {
-        if (numSteps < numBeats) {
-            std::cout << "Error: numBeats must be less than numSteps" << std::endl;
-            return "0";
-        }
-        
+        const int numBeats = numSteps * beatRatio;
         std::string x = "1";
         int numX = numBeats;
         std::string y = "0";
         int numY = numSteps - numBeats;
-        
         do {
             const std::string tmp_x = x;
             const std::string tmp_y = y;
@@ -47,34 +42,58 @@ public:
         for (int i = 0; i < numX; ++i) seq += x;
         for (int i = 0; i < numY; ++i) seq += y;
         
-        for(unsigned int i = 0; i < std::min(numSteps, maxStepSize); ++i) {
+        for(unsigned int i = 0; i < std::min(numSteps, MaxNumSteps); ++i) {
             if(seq[i] == "1") {
                 rhythmPattern.set(i);
             }else{
                 rhythmPattern.reset(i);
             }
         }
-        
-        StepSize = numSteps;
     }
     
-    int getStepSize(){
-        return StepSize.load();
-    }
-    
-    bool nextStep(){
-        if(currentStep >= StepSize) {
-            currentStep = 0;
+    bool update(){
+        sampleCounter++;
+        if(sampleCounter > stepWidthInSamples) {
+            stepPosition++;
+            if(stepPosition > numSteps) stepPosition = 0;
+            sampleCounter = 0;
+            return rhythmPattern.test(currentStep);
+        } else if(sampleCounter < gateWidthInSamples) {
+            return rhythmPattern.test(stepPosition);
+        } else {
+            return false;
         }
-        
-        return rhythmPattern.test(currentStep);
+    }
+    
+    void setnumSteps(const unsigned int step) {
+        if(MaxNumSteps < step) {
+            std::cout<<"Error: numSteps must be less than 128"<<std::endl;
+            return;
+        }
+        numSteps = step;
+    }
+    
+    void setBeatRatio(const float ratio) {
+        if(ratio < 0.0f || 1.0f < ratio) {
+            std::cout<<"Error: beatRatio must be in the range 0~1"<<std::endl;
+            return;
+        }
+        beatRatio.store(ratio);
+    }
+    
+    void setBPM(const float bpm, const int digitalSampleRate) {
+        stepWidthInSamples = bpm / (digitalSampleRate * 60.0f);
     }
     
 private:
-    static constexpr int maxStepSize = 32;
-    std::bitset<maxStepSize> rhythmPattern{0};
-    std::atomic<int> StepSize{0};
-    std::atomic<int> currentStep{0};
+    static constexpr int MaxNumSteps = 127;//シーケンサーの最大ステップ数
+    static constexpr int GateWidthInSamples = 50;//Gate信号ONの長さ(サンプル数)
+    std::bitset<MaxNumSteps> rhythmPattern{0};//シーケンスパターン
+    std::atomic<int> numSteps{0};//現在のステップ数
+    int stepPosition = 0;//シーケンサーのステップ位置
+    std::atomic<float> beatRatio{0.0f};//シーケンス内のトリガーする割合(0.0f~1.0f)
+    std::atomic<int> stepWidthInSamples{100};//1ステップの長さ（サンプル数）
+    int sampleCounter = 0;//1ステップの長さのカウンター
 };
 
 #endif /* EuclideanRhythm_h */
